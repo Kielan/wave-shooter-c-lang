9c04cf2c6c4f91b4d2fc2a2f4f021f70fac4a595
Kielan <ki.lemons@gmail.com>
2022-11-07 20:42:37 -0500

    Update wm.c
    window manager main events init.

--- source/dust/windowmanager/intern/wm.c
+++ source/dust/windowmanager/intern/wm.c
@@ -1,2 +1,30 @@
 #include <stddef.h>
 #include <string.h>
+
+#include "BKE_context.h"
+
+#include "WM_api.h"
+#include "wm_draw.h"
+#include "wm_event_system.h"
+
+void WM_main(bContext *C)
+{
+  /* Single refresh before handling events.
+   * This ensures we don't run operators before the depsgraph has been evaluated. */
+  wm_event_do_refresh_wm_and_depsgraph(C);
+
+  while (1) {
+
+    /* Get events from ghost, handle window events, add to window queues. */
+    wm_window_process_events(C);
+
+    /* Per window, all events to the window, screen, area and region handlers. */
+    wm_event_do_handlers(C);
+
+    /* Events have left notes about changes, we handle and cache it. */
+    wm_event_do_notifiers(C);
+
+    /* Execute cached changes draw. */
+    wm_draw_update(C);
+  }
+}
