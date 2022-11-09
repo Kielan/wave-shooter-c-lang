ee620d576df3c56f976f1d6d7d0f6689043168e3
Kielan <ki.lemons@gmail.com>
2022-11-09 16:32:31 -0500

    Update wm_api.h

--- source/dust/windowmanager/wm_api.h
+++ source/dust/windowmanager/wm_api.h
@@ -1,19 +1,15 @@
 #pragma once
 
-/** \file
- * \ingroup wm
- *
- * \page wmpage windowmanager
- * \section wmabout About windowmanager
- * \ref wm handles events received from \ref GHOST and manages
+/*
+ * windowmanager
+ * section wmabout About windowmanager
+ * ref wm handles events received from \ref GHOST and manages
  * the screens, areas and input for Blender
- * \section wmnote NOTE
- * \todo document
+ * section wmnote NOTE
+ * todo document
  */
-
-/* dna-savable wmStructs here */
-#include "BLI_compiler_attrs.h"
-#include "DNA_windowmanager_types.h"
+#include "LIB_compiler_attrs.h"
+#include "structs_windowmanager_types.h"
 #include "WM_keymap.h"
 
 #ifdef __cplusplus
